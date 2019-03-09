a4224a0f3707ea897cb9c3c8bc1078bea630c10b
maximys963 <maxim.p.jsdev@gmail.com>
2019-03-09 18:59:28 +0200

    Updates

---
+++ precache-manifest.6cf05d147f90c77937638ba58dc1a574.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f02041ec73d76f978ac6",
+    "url": "/Task-Manager/static/js/runtime~main.f02041ec.js"
+  },
+  {
+    "revision": "11ea21772837eb94c008",
+    "url": "/Task-Manager/static/js/main.11ea2177.chunk.js"
+  },
+  {
+    "revision": "54d3e44eab4f5b8509b5",
+    "url": "/Task-Manager/static/js/2.54d3e44e.chunk.js"
+  },
+  {
+    "revision": "11ea21772837eb94c008",
+    "url": "/Task-Manager/static/css/main.4c7fc1ce.chunk.css"
+  },
+  {
+    "revision": "9113e455ca0e24aa6e9bc8f8fda6e90a",
+    "url": "/Task-Manager/index.html"
+  }
+];
